c0841d64b2186faa8e23a37930fcea0b6214d074
jonathan bevilacqua <jbevilacqua@unistra.fr>
2025-05-12 19:13:16 +0200

    resolution de probleme interne avec git

---
+++ src/stuff/Equipement.java
@@ -0,0 +1,15 @@
+package stuff;
+
+public class Equipement {
+    boolean actif=false;
+    String nom;
+    String description;
+    public boolean estEquipe() {
+        return actif;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+}
+
