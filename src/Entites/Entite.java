68b35fffeda3cea451045f365b779e9874c50d69
jonathan bevilacqua <jbevilacqua@unistra.fr>
2025-05-15 14:43:00 +0200

    update de entite

--- src/Entites/Entite.java
+++ src/Entites/Entite.java
@@ -1,8 +1,9 @@
 package Entites;
 
-import java.util.Scanner;
 import static Partie.De.lancerDe;
+
 import donjons.Position;
+import donjons.Donjon;
 
 public class Entite {
     private Position m_position;
@@ -20,7 +21,7 @@ public class Entite {
     public boolean est_mort(){
         //verifie si l'entite qui vient d'etre attaquer a toujours des points de vie restant
         if(this.m_statistiques.getPv()<=0){
-            String phrase=this.getNom() +  "est mort";
+            affichePhrase(this.getNom() +  "est mort");
             //Affichage
             //Sortir l'entite de la liste
             return true;
@@ -38,7 +39,8 @@ public class Entite {
         // Sauvegarde des positions initiales pour les restaurer en cas d'annulation
 
         int deplacementsRestants = this.m_statistiques.getVitesse() / 3;
-
+        int initY= this.m_position.getY();
+        int initX= this.m_position.getX();
         affichePhrase("Entrez une direction (z = haut, q = gauche, s = bas, d = droite, o= retour à l'origine) :");
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
@@ -46,43 +48,64 @@ public class Entite {
         for (int i = 0; i < deplacementsRestants; i++) {
             System.out.print("Déplacement " + (i + 1) + "/" + deplacementsRestants + " : ");
             char direction = scanner.next().charAt(0);
-            int initY= this.m_position.getY();
-            int initX= this.m_position.getX();
-
+            int oldY= this.m_position.getY();
+            int oldX= this.m_position.getX();
+            String erreur= "entite sur le chemin ou passage hors de la carte ";
             switch (direction) {
                 case 'z':
-                    this.m_position.changeY(this.m_position.getY() - 1);
+                    if(deplacementEstPossible(this.m_position.getX(),this.m_position.getY()-1)){
+                        this.m_position.changeY(this.m_position.getY() - 1);
+                    }
+                    else{
+                        i--;
+                        affichePhrase(erreur);
+                    }
+                    break;
                 case 's':
-                    this.m_position.changeY(this.m_position.getY() + 1);
+                    if(deplacementEstPossible(this.m_position.getX(),this.m_position.getY()+1)){
+                        this.m_position.changeY(this.m_position.getY() + 1);
+                    }
+                    else{
+                        i--;
+                        affichePhrase(erreur);
+                    }
                 case 'q':
-                    this.m_position.changeX(this.m_position.getX() - 1);
+                    if(deplacementEstPossible(this.m_position.getX()-1,this.m_position.getY())){
+                        this.m_position.changeX(this.m_position.getX() - 1);
+                    }
+                    else{
+                        i--;
+                        affichePhrase(erreur);
+                    }
+
                 case 'd':
-                    this.m_position.changeX(this.m_position.getX() + 1);
+                    if(deplacementEstPossible(this.m_position.getX() + 1,this.m_position.getY())){
+                        this.m_position.changeX(this.m_position.getX() + 1);
+                    }
+                    else{
+                        i--;
+                        affichePhrase(erreur);
+                    }
                 case 'o':
                     this.m_position.changeXY(initX,initY);
                 default:
                     System.out.println("Direction invalide ! Entrez z, q, s ,d ou o uniquement.");
                     i--; // Annule cette itération, car le déplacement n'a pas eu lieu
             }
-            System.out.println("Nouvelle position : (" + positionX + ", " + positionY + ")");
+
+            //on transforme la position numérique en alphabétique avec 0=A et 26=Z
+            affichePhrase("Nouvelle position : (" + this.changeEntierEnLettre(this.m_position.getX()) + ", " + this.m_position.getY() + ")");
         }
-
-        //changer cette partie quand on aura fais les liste de monstre et d'obstacle
-        if (positionX < 0 || positionY < 0) {
-            // Si la position finale est invalide, annuler tous les déplacements
-            System.out.println("Position finale invalide! Revenir à la position de départ.");
-
-            // Retourner à la position de départ
-            positionX = positionInitialeX;
-            positionY = positionInitialeY;
-            System.out.println("Tous vos déplacements ont été annulés. Essayez une autre approche!");
-
-
-        }
+    }
+    public char changeEntierEnLettre(int number) {
+        return (char) ('A'+number);
+    }
+    public boolean deplacementEstPossible(int x, int y, Donjon donjon){
+        return !(donjon.contientObstacle(x, y) || donjon.contientEntite(x, y) || x <= 0 || y <= 0);
     }
 
     public void attaquer(Entite cible) {
-         //verifier si la cible est l'assaillant ou plus ou moins d'une case de distance ,
+        //verifier si la cible est l'assaillant ou plus ou moins d'une case de distance ,
         // si cest 1 case ou moins on ajoute la force au resultat du lancer , si c'est plus c'est la dexterite
         int degattotaux=0;
         String[] decomposeDe = getDegat().split("d"); // ["3", "4"]
@@ -104,14 +127,6 @@ public class Entite {
     }
 
 
-    public String getType(){
-        return "";
-    }
-
-    public Position getPosition(){
-        return m_position;
-    }
-
     public String getNom() {
         return m_nom;
     }
@@ -122,3 +137,4 @@ public class Entite {
         return "";
     }
 }
+
