05c03d3d6b4652548eb833739b7e04ff78484bfc
jbevilacqua <jonathan.bevilacqua@etu.unistra.fr>
2025-05-28 11:50:14 +0200

    maj code

--- src/partie/Mj.java
+++ src/partie/Mj.java
@@ -24,6 +24,50 @@ public class Mj {
     }
 
 
+    public Personnage creerPersonnage(){
+        String nom= demanderString("Entrez un nom de personnage :\n");
+        afficherPhrase("Choisir votre classe parmis:\n");
+        afficherPhrase("1/ Clerc:\n");
+        afficherPhrase("2/ Guerrier:\n");
+        afficherPhrase("3/ Roublard:\n");
+        afficherPhrase("4/ Magicien:\n");
+        CharClasse classe= new Clerc();
+        switch (demanderInt("Choisir votre classe parmis:\n")){
+            case 1:
+                classe= new Clerc();
+                break;
+            case 2:
+                classe= new Guerrier();
+                break;
+            case 3:
+                classe= new Roublard();
+                break;
+            case 4:
+                classe= new Magicien();
+                break;
+        }
+
+        afficherPhrase("Choisir votre Race parmis:\n");
+        afficherPhrase("1/ Humain:\n");
+        afficherPhrase("2/ Elfe:\n");
+        afficherPhrase("3/ Halfelin:\n");
+        afficherPhrase("4/ Nain:\n");
+        Race race= new Humain();
+        switch (demanderInt("Choisir votre classe parmis:\n")){
+            case 1:
+                race= new Humain();
+                break;
+            case 2:
+                race= new Elfe();
+                break;
+            case 3:
+                race= new Halfelin();
+                break;
+            case 4:
+                race= new Nain();
+                break;
+        }
+        return new Personnage(nom,race,classe);
     }
 
     public List<Equipement> creerEquipementAuSol(int nbe) {
