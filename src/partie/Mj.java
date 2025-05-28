c7fdda297360b1cc6d9eaaf0873c04afd614648d
jbevilacqua <jonathan.bevilacqua@etu.unistra.fr>
2025-05-28 11:32:37 +0200

    maj code

--- src/partie/Mj.java
+++ src/partie/Mj.java
@@ -1,14 +1,20 @@
 package partie;
 
 import entites.*;
+import equipements.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import donjons.Donjon;
 
 import static partie.Affichage.*;
 
 public class Mj {
 
     private List<Personnage> m_joueurs;
+    private List<Monstre> m_monstre;
+
     public Mj() {
         int nbJoueur= demanderInt("quel taille fais notre groupe d'aventurier ?\n");
         for (int i = 0; i < nbJoueur; i++) {
@@ -63,8 +69,52 @@ public class Mj {
         return new Personnage(nom,race,classe);
     }
 
-    public void lancerPartie(){
+    public List<Equipement> creerEquipementAuSol(int nbe) {
+        List<Equipement> res = new ArrayList<>();
+        String[] tabstuff = {
+                "baton", "masse d'armes", "arc court", "arbalete legere", "fronde",
+                "epee longue", "rapiere", "epee à deux mains","cotte de mailles", "harnois"
+        };
+        Equipement nouvelleArme = null;
+        for (int i = 0; i < nbe; i++) {
+            afficherPhrase("Quelle est le nom de l'arme parmi celles-ci ?");
+            for (int j = 0; j < tabstuff.length; j++) {
+                afficherPhrase("arme " + j + " = " + tabstuff[j]);
+            }
 
+            int numero=-1;
+            while (numero < 0 || numero >= tabstuff.length){
+                numero = demanderInt("Veuillez entrer un numéro entre 0 et " + (tabstuff.length - 1));
+            }
+            switch (numero){
+                case 1,2 :
+                    nouvelleArme = new ArmeCourante(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                case 5,3,4 :
+                    nouvelleArme = new ArmeDistante(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                case 8,6,7:
+                    nouvelleArme = new ArmeGuerre(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                case 9,10 :
+                    nouvelleArme = new ArmureLegere(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                case 11,12 :
+                    nouvelleArme = new ArmureLourde(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+            }
+        res.add(nouvelleArme);
+        }
+        return res;
+    }
+
+    public int creerObstacle(int nbo){
+        
+    }
+
+
+    public void lancerPartie(){
+        int x=demanderInt("quelle taille fera votre donjon en longueur (x)");
+        int y=demanderInt("quelle taille fera votre donjon en largeur (y)");
+        List<Equipement> equipements=creerEquipementAuSol(demanderInt("combien d'objet ramassable seront au sol pour les joueurs"));
+
+
+        Donjon d=new Donjon()
     }
 
 }
