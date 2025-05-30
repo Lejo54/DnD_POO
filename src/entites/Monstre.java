0e68aa5848a0c2e691a4b124d43ba25c2be84aba
Joseph Cuiller <jcuiller@etu.unistra.fr>
2025-05-30 17:48:52 +0200

    update src: création de donjon, de partie, de monstre

--- src/entites/Monstre.java
+++ src/entites/Monstre.java
@@ -1,23 +1,35 @@
 package entites;
 
-public class Monstre {
-    String espece;
-    int numero;
-    int portee;
-    String degat;
-    int class_d_armure;
+public class Monstre extends Entite {
+    String m_espece;
+    int m_numero;
+    int m_portee;
+    String m_degat;
+    int m_class_armure;
 
-    public String getNom() {
-        return espece + " " + numero;
+    public Monstre(String espece, int numero, int portee, String degat) {
+        m_espece = espece;
+        m_numero = numero;
+        m_portee = portee;
+        m_degat = degat;
     }
+    public String getNom() {
+        return m_espece + " " + m_numero;
+    }
+
+    @Override
+    public int getArmure() {
+        return 0;
+    }
+
     public int getarmure(){
-        return class_d_armure;
+        return m_class_armure;
     }
     public String getDegat() {
-        return degat;
+        return m_degat;
     }
     public String getType() {
         return "Monstre";
     }
-    public int getPortee(){return portee;}
+    public int getPortee(){return m_portee;}
 }
--- src/partie/Partie.java
+++ src/partie/Partie.java
@@ -1,6 +1,8 @@
 package partie;
 import donjons.Donjon;
+import donjons.Obstacle;
 import entites.*;
+import equipements.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,27 +22,13 @@ public class Partie {
             joueurs.add(creerJoueurs());
         }
         m_joueurs.addAll(joueurs);
-
+        m_donjons.add(creerDonjon());
     }
     // Getters
     public List<Personnage> getJoueurs() {
         return m_joueurs;
     }
 
-    public void trierParInitiative(Entite[] entites) {
-        int n = entites.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (entites[j].getStatistiques().getInitiative() < entites[minIndex].getStatistiques().getInitiative()) {
-                    minIndex = j;
-                }
-            }
-            Entite temp = entites[i];
-            entites[i] = entites[minIndex];
-            entites[minIndex] = temp;
-        }
-    }
     public Personnage creerJoueurs(){
         String nom= demanderString("Entrez un nom de personnage :\n");
         afficherPhrase("Choisir votre classe parmis:\n");
@@ -87,4 +75,92 @@ public class Partie {
         return new Personnage(nom,race,classe);
     }
 
+    public Donjon creerDonjon(){
+        //taille de la map
+        int x=demanderInt("Entrez la longeur de votre donjon (entre 15 et 25):\n");
+        int y=demanderInt("Entrez la hauteur de votre donjon (entre 15 et 25):\n");
+
+        //Création des obstacles
+        List<Obstacle> obstacle=creerObstacle(demanderInt("combien d'obstacle se trouveront sur la carte ?\n"));
+
+        //Création des objets ramassables
+        List<Equipement> objetAuSol=creerEquipementAuSol(demanderInt("combien d'objet ramassable seront au sol pour les joueurs\n"));
+
+        //Création des monstres
+        List<Entite> entites= new ArrayList<>();
+        entites.addAll(getJoueurs());
+        int nbMonstre=demanderInt("Entrez le nombre de monstre présent dans le donjon:\n");
+        entites.addAll(creerMonstre(nbMonstre));
+
+        Donjon donjon=new Donjon(objetAuSol,entites,obstacle,x,y);
+        return donjon;
+    }
+
+    public List<Monstre> creerMonstre(int nbMonstre){
+        List<Monstre> monstres= new ArrayList<>();
+        for (int i = 0; i < nbMonstre; i++) {
+            String espece= demanderString("Entrez le nom de l'espèce du monstre:\n");
+            int numéro= demanderInt("Entrez le numero de monstre:\n");
+            int portee= demanderInt("Entrez la portée de l'attaque du monstre (1 pour une attaque au corps à corps):\n");
+            String Degat=creerDegat();
+            //On créé le monstre et on l'ajoute dans la liste des monstres
+            monstres.add(new Monstre(espece,numéro,portee,Degat));
+        }
+
+        return monstres;
+    }
+    public String creerDegat(){
+        String nbFace=String.valueOf(demanderInt("Entrez le nombre de face de votre dé de dégat:\n"));
+        String nbLancer=String.valueOf(demanderInt("Entrez le numero de lancer de votre dé de dégat:\n"));
+        String de=nbLancer+"d"+nbFace;
+        return de;
+    }
+    public List<Obstacle> creerObstacle(int nbo){
+        List<Obstacle> res=new ArrayList<>();
+        for(int i =0 ; i<nbo ; i++){
+            int x =demanderInt("position x de votre obstacle ?\n");
+            int y =demanderInt("position y de votre obstacle ?\n");
+            res.add(new Obstacle(x,y));
+        }
+        return res;
+    }
+
+    public List<Equipement> creerEquipementAuSol(int nbe) {
+        List<Equipement> res = new ArrayList<>();
+        String[] tabstuff = {
+         "baton", "masse d'armes", "arc court", "arbalete legere", "fronde",
+         "epee longue", "rapiere", "epee à deux mains","cotte de mailles", "harnois"
+        };
+        Equipement nouvelleArme=null;
+        for (int i = 0; i < nbe; i++) {
+            afficherPhrase("Quelle est le nom de l'arme parmi celles-ci ?");
+            for (int j = 0; j < tabstuff.length; j++) {
+                afficherPhrase("arme " + j + " = " + tabstuff[j]);
+            }
+
+            int numero=-1;
+            while (numero < 0 || numero >= tabstuff.length){
+                numero = demanderInt("Veuillez entrer un numéro entre 0 et " + (tabstuff.length - 1));
+            }
+            switch (numero){
+                case 1,2 :
+                    nouvelleArme = new ArmeCourante(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                    break;
+                case 5,3,4 :
+                    nouvelleArme = new ArmeDistante(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                    break;
+                case 8,6,7:
+                    nouvelleArme = new ArmeGuerre(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                    break;
+                case 9,10 :
+                    nouvelleArme = new ArmureLegere(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                    break;
+                case 11,12 :
+                    nouvelleArme = new ArmureLourde(tabstuff[numero],demanderString("une description pour cette armes?(sinon appuyer sur entree)"),false);
+                    break;
+            }
+            res.add(nouvelleArme);
+        }
+        return res;
+    }
 }
